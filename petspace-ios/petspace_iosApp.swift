ba4b4293e7d1798251a724e7f24d0ec9f0d9e3ce
Hoeun Lee <hoeunlee228@gmail.com>
2023-10-14 18:18:48 +0900

    Create a Xcode Project

---
+++ petspace-ios/petspace_iosApp.swift
@@ -0,0 +1,20 @@
+//
+//  petspace_iosApp.swift
+//  petspace-ios
+//
+//  Created by Hoeun Lee on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct petspace_iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
