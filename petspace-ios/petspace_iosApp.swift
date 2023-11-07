cc5f07f787f367857bc5d2cc3808bddd29906c5b
Hoeun Lee <hoeunlee228@gmail.com>
2023-11-07 22:58:06 +0900

    initial commit

---
+++ petspace-ios/petspace_iosApp.swift
@@ -0,0 +1,20 @@
+//
+//  petspace_iosApp.swift
+//  petspace-ios
+//
+//  Created by Hoeun Lee on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct petspace_iosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
